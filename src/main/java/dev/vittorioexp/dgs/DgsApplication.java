8bfc1912ec30d48a64fe06e73af1dd4d95394b42
Vittorio <vi.esposito@reply.it>
2022-09-21 08:37:27 +0200

    initial commit

---
+++ src/main/java/dev/vittorioexp/dgs/DgsApplication.java
@@ -0,0 +1,13 @@
+package dev.vittorioexp.dgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DgsApplication.class, args);
+	}
+
+}
